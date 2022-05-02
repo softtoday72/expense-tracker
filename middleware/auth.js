f5a3d7a8b0af840bd92005648005a44562f86afe
softtoday72 <demonhunter720525@hotmail.com>
2022-05-02 22:26:14 +0800

    fix seeder

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,8 @@
 module.exports = {
   authenticator: (req, res, next) => {
     if (req.isAuthenticated()) {
-      console.log('----登入----')
       return next()
     }
-    
-    console.log('----無法登入----')
     req.flash('warning_msg', '請先登入才能使用！')
     res.redirect('/users/login')
   }
--- models/seed/user_seeder.js
+++ models/seed/user_seeder.js
@@ -5,29 +5,41 @@ const User = require('../../models/users')
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 const db = require('../../config/mongoose')
-const { userList, recordList } = require('./user_seeder.json')
 
-db.once('open', ()=> {
-  Promise.all(Array.from(userList, (seedUser, i) => {
-    return User.create({
-      name:seedUser.name, email: seedUser.email, password: seedUser.password
+
+
+const SEED_USER = { 
+  name: 'User1', email: 'user1@example.com', password: '12345678' 
+}
+
+const SEED_TRACKER = [
+  { name: "早餐", date: "2022-03-11", amount: 100, catrgory: "交通出行" },
+  { name: "管理費", date: "2022-03-12", amount: 200, catrgory: "居家物業" }
+]
+
+
+
+
+db.once("open", () => {
+   User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+     password: SEED_USER.password
     })
     .then(user => {
       const userId = user._id
-      return Promise.all(Array.from(recordList, (_, i) => {
-        Category.findOne({ id : recordList[i].categoryId })
-          .then(userCategory => {
-            const categoryId = userCategory.name
-            return Record.create({
-              name: recordList[i].name, date: recordList[i].date, amount: recordList[i].amount, category: recordList[i].catrgory, userId, categoryId
-          })
-        
-          })
-      }))
+      return Promise.all(Array.from(
+        { length: 2 },
+        (_, i) =>
+          Category.findOne({ category: SEED_TRACKER[i].catrgory })
+            .then(userCategory => {
+              const categoryId = userCategory.id
+              return Record.create({ name: SEED_TRACKER[i].name, date: SEED_TRACKER[i].date, amount: SEED_TRACKER[i].amount, category: SEED_TRACKER[i].catrgory, userId, categoryId })
+            })
+      ))
     })
-      .then(() => {
-        console.log("done.")
-        process.exit()
-      })
-  }))
+    .then(() => {
+      console.log("done.")
+      process.exit()
+    })
 })
